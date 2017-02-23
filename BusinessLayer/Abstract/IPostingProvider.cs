90f76630b5fee447007fd3d8f7cc0111ced16eb3
Marius Aiordăchioaei <maiordachioaei@pentalog.fr>
2017-02-23 10:18:08 +0200

    Added Post provider

---
+++ BusinessLayer/Abstract/IPostingProvider.cs
@@ -0,0 +1,16 @@
+﻿using Model.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Abstract {
+    interface IPostingProvider {
+
+        File CreateFile (string fileName, string mimeType, byte[] content);
+
+        Post CreatePost (string authorId, string body, File attachment = null);
+
+    }
+}
---
+++ BusinessLayer/Concrete/PostingProvider.cs
@@ -0,0 +1,48 @@
+﻿using BusinessLayer.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Model.Models;
+using DataAccessLayer.Abstract;
+using System.Web;
+
+namespace BusinessLayer.Concrete {
+    class PostingProvider : IPostingProvider {
+
+        public IDbContext DbContext { get; protected set; }
+        public PostingProvider(IDbContext dbContext) {
+            this.DbContext = dbContext;
+        }
+
+        public File CreateFile (HttpPostedFileBase file) {
+            byte[] content = new byte[file.ContentLength];
+            file.InputStream.Read (content, 0, file.ContentLength);
+            return this.CreateFile (file.FileName, file.ContentType, content);
+        }
+
+        public File CreateFile (string fileName, string mimeType, byte[] content) {
+            var newFile = new File {
+                Content = content,
+                FileName = fileName,
+                MimeType = mimeType
+            };
+
+            DbContext.Files.Add (newFile);
+            return newFile;
+        }
+
+        public Post CreatePost (string authorId, string body, File attachment = null) {
+            var newPost = new Post {
+                CreateTimestamp = DateTime.Now,
+                Author = DbContext.Users.FirstOrDefault (u => u.Id == authorId),
+                Body = body,
+                Attachment = attachment
+            };
+
+            DbContext.Posts.Add (newPost);
+            return newPost;
+        }
+    }
+}
